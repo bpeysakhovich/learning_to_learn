18061b5071c20ff43a779390307b0995a17b26ca
Nicolas Masse <nicolas.masse@gmail.com>
2018-05-19 21:47:20 -0500

    Initial commit

---
+++ convolutional_layers.py
@@ -0,0 +1,105 @@
+import numpy as np
+import tensorflow as tf
+import pickle
+import task
+import os
+
+
+# TODO: move these parameters to a better home
+filters = [32,32,64,64]
+kernel_size = [3, 3]
+pool_size = [2,2]
+stride = 1
+num_layers = len(filters)
+dense_layers = [4096, 2000, 1000] # 4096 is the size after the convolutional layers
+drop_keep_pct = 0.8
+batch_size = 256
+training_iterations = 40000
+learning_rate = 0.001
+weights_save_file = './savedir/' + 'conv_weights_test.pkl'
+image_dataset = 'imagenet'
+
+use_gpu = True
+if use_gpu:
+    gpu_id = '0'
+    os.environ["CUDA_VISIBLE_DEVICES"] = gpu_id
+
+
+def train_weights():
+
+    # Reset TensorFlow graph
+    tf.reset_default_graph()
+
+    # Create placeholders for the model
+    input_data   = tf.placeholder(tf.float32, [batch_size, 32, 32, 3], 'input')
+    target_data  = tf.placeholder(tf.float32, [batch_size, dense_layers[-1]], 'target')
+
+    # pass input through convolutional layers
+    x = apply_convulational_layers(input_data, None)
+    x = tf.reshape(x, [batch_size, -1])
+
+    # pass input through dense layers
+    with tf.variable_scope('dense_layers'):
+        c = 0.1
+        W0 = tf.get_variable('W0', initializer = tf.random_uniform([dense_layers[0], dense_layers[1]], -c, c), trainable = True)
+        W1 = tf.get_variable('W1', initializer = tf.random_uniform([dense_layers[1], dense_layers[2]], -c, c), trainable = True)
+        b0 = tf.get_variable('b0', initializer = tf.zeros([1, dense_layers[1]]), trainable = True)
+        b1 = tf.get_variable('b1', initializer = tf.zeros([1, dense_layers[2]]), trainable = True)
+
+    x = tf.nn.relu(tf.matmul(x, W0) + b0)
+    y = tf.matmul(x, W1) + b1
+
+    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = y, labels = target_data, dim = 1))
+    optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate)
+    train_op = optimizer.minimize(loss)
+
+    # we will train the network on imagenet dataset
+    stim = task.Stimulus(image_dataset)
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+
+        for i in range(training_iterations):
+
+            batch_data, batch_labels = stim.generate_image_batch()
+            _, train_loss  = sess.run([train_op, loss], feed_dict = {input_data: batch_data, target_data: batch_labels})
+
+            if i%1000 == 0:
+                print('Iteration ', i, ' Loss ', train_loss)
+
+        W = {}
+        for var in tf.trainable_variables():
+            W[var.op.name] = var.eval()
+
+        pickle.dump(W, open(weights_save_file,'wb'))
+        print('Convolutional weights saved in ', weights_save_file)
+
+
+def apply_convulational_layers(x, saved_weights_file):
+
+    # load previous weights is saved_weights_file is not None,
+    # otherwise, train new weights
+    if saved_weights_file is None:
+        kernel_init = [None for _ in range(num_layers)]
+        bias_init = [tf.zeros_initializer() for _ in range(num_layers)]
+        train = True
+    else:
+        kernel_names = ['conv2d']
+        for i in range(1, num_layers):
+            kernel_names.append('conv2d_' + str(i))
+        conv_weights = pickle.load(open(saved_weights_file,'rb'))
+        kernel_init = [tf.constant_initializer(conv_weights[k + '/kernel']) for k in kernel_names]
+        bias_init = [tf.constant_initializer(conv_weights[k + '/bias']) for k in kernel_names]
+        train = False
+
+    for i in range(num_layers):
+
+        x = tf.layers.conv2d(inputs = x, filters = filters[i], kernel_size = kernel_size, kernel_initializer = kernel_init[i],  \
+            bias_initializer = bias_init[i], strides = stride, activation = tf.nn.relu, padding = 'SAME', trainable = train)
+
+        if i > 0 and i%2 == 1:
+            # apply max pooling and dropout after every second layer
+            x = tf.layers.max_pooling2d(inputs = x, pool_size = pool_size, strides = 2, padding='SAME')
+            x = tf.nn.dropout(x, drop_keep_pct)
+
+    return x
---
+++ task.py
@@ -0,0 +1,107 @@
+import numpy as np
+from parameters import *
+import pickle
+import matplotlib.pyplot as plt
+
+# TODO: move these parameters to a better home
+fix = 20
+stim = 20
+delay = 10
+resp = 10
+noise_sd = 0.1
+trial_length = fix + stim + delay + resp
+par['batch_size'] = 256
+par['layer_dims'] = [1000]
+
+
+class Stimulus:
+
+    def __init__(self, task_name):
+
+        # we will train the convolutional layers using the training images
+        # we will use the train images fro the learning to learn experiments
+        self.imagenet_dir = './ImageNet/'
+        self.load_imagenet_data()
+
+        # for the simple image/saccade task (task 1), select 50 pairs of images
+        # TODO: find better name than task1
+        self.image_list_task1 = np.random.choice(len(self.test_labels), size = (50,2), replace = False)
+
+
+    def generate_batch_task1(self, image_pair):
+
+        batch_data   = np.zeros((par['batch_size'], trial_length, 32,32,3), dtype = np.float32)
+        rewards = np.zeros((par['batch_size'], trial_length, par['layer_dims'][-1]), dtype = np.float32)
+
+        for i in range(par['batch_size']):
+            sac_dir = np.random.choice(2)
+            image_ind = self.image_list_task1[image_pair, 0] if sac_dir == 0 else self.image_list_task1[image_pair, 1]
+            batch_data[i, range(fix, fix+stim), :, :, :] = np.reshape(self.test_images[image_ind,:,:,:], (1,32,32,3))
+            # fixation
+            rewards[i,range(0, fix+stim+delay), 1] = -1 # fixation break
+            rewards[i,range(0, fix+stim+delay), 2] = -1 # fixation break
+            # response
+            batch_labels[i,range(fix + stim + delay, trial_length), sac_dir] = 1 # reward correct response
+            batch_labels[i,range(fix + stim + delay, trial_length), 1+sac_dir%2] = -1 # penalize incorrect response
+
+        batch_data += np.random.normal(0, noise_sd, size = batch_data.shape)
+
+        return np.maximum(0, batch_data), rewards
+
+
+
+    def load_imagenet_data(self):
+
+        """
+        Load ImageNet data
+        """
+        self.train_images = np.array([])
+        self.train_labels = np.array([])
+
+        for i in range(10):
+            x =  pickle.load(open(self.imagenet_dir + 'train_data_batch_' + str(i+1),'rb'))
+            self.train_images = np.vstack((self.train_images, x['data'])) if self.train_images.size else x['data']
+            labels = np.reshape(np.array(x['labels']),(-1,1))
+            self.train_labels = np.vstack((self.train_labels, labels))  if self.train_labels.size else labels
+
+        x =  pickle.load(open(self.imagenet_dir + 'val_data','rb'))
+        self.test_images = np.array(x['data'])
+        self.test_labels = np.reshape(np.array(x['labels']),(-1,1))
+
+
+    def generate_cifar_tuning(self):
+
+        """
+        Load CIFAR-100 data
+        """
+        x = pickle.load(open(self.cifar100_dir + 'train','rb'), encoding='bytes')
+
+        self.train_images = np.array(x[b'data'])
+        self.train_labels = np.array(np.reshape(np.array(x[b'fine_labels']),(-1,1)))
+
+        x = pickle.load(open(self.cifar100_dir + 'test','rb'), encoding='bytes')
+
+        self.test_images  = np.array(x[b'data'])
+        self.test_labels  = np.array(np.reshape(np.array(x[b'fine_labels']),(-1,1)))
+
+
+    def generate_image_batch(self, test = False):
+
+        # Select example indices
+        random_selection = np.random.randint(0, len(self.train_labels), par['batch_size'])
+
+        # Pick out batch data and labels
+        batch_data   = np.zeros((par['batch_size'], 32,32,3), dtype = np.float32)
+        batch_labels = np.zeros((par['batch_size'], par['layer_dims'][-1]), dtype = np.float32)
+
+        for i, image_index in enumerate(random_selection):
+            if test:
+                k = self.test_labels[image_index] - 1
+                batch_labels[i, k] = 1
+                batch_data[i, :] = np.float32(np.reshape(self.test_images[image_index, :],(1,32,32,3), order='F'))/255
+            else:
+                k = self.train_labels[image_index] - 1
+                batch_labels[i, k] = 1
+                batch_data[i, :] = np.float32(np.reshape(self.train_images[image_index, :],(1,32,32,3), order='F'))/255
+
+        return batch_data, batch_labels
